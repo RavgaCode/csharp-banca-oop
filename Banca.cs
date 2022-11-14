1dd8a47f372ccf2958c2e9d297358a5547a5c149
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-11-14 19:46:46 +0100

    Basic setup classes

---
+++ Banca.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Banca
+{
+    public string Nome { get; set; }
+    public List<Cliente> ListaClienti { get; set; }
+    public List<Prestito> ListaPrestiti { get; set; }
+
+    public Banca (string nome)
+    {
+        Nome = nome;
+        ListaClienti = new List<Cliente> ();
+        ListaPrestiti = new List<Prestito> ();
+    }
+}
---
+++ Prestito.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Prestito
+{
+    public int ID { get; set; }
+    public string Intestatario { get; set; }
+    public int Ammontare { get; set; }
+    public float Rata { get; set; }
+    public DateTime DataInizio { get; set; }
+    public DateTime DataFine  { get; set; }
+
+    public Prestito (int iD, string intestatario, int ammontare, float rata, DateTime dataInizio, DateTime dataFine)
+    {
+        ID = iD;
+        Intestatario = intestatario;
+        Ammontare = ammontare;
+        Rata = rata;
+        DataInizio = dataInizio;
+        DataFine = dataFine;
+    }
+}
